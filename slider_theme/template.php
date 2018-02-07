428fde4640e7e8dcd2021ec16d770cab5c20f42c
Konstantin Ignatiev <QfpbC7u3V13qJUop@i2pmail.org>
2018-02-08 00:59:30 +0300

    [WIP] Header.

--- slider_theme/template.php
+++ slider_theme/template.php
@@ -1,5 +1,9 @@
 <?php
 
-function bartik_preprocess_html(&$variables) {
+function slidertheme_preprocess_html(&$variables) {
   drupal_add_css(path_to_theme() . '/css/bgs.css', array('group' => CSS_THEME, 'browsers' => array('IE' => TRUE, '!IE' => TRUE), 'preprocess' => FALSE));
+}
+
+function slider_theme_process_page(&$variables) {
+    $variables['logo'] =  'images/logo.jpg';
 }
--- slider_theme/templates/page.tpl.php
+++ slider_theme/templates/page.tpl.php
@@ -2,45 +2,49 @@
 <div id="page-wrapper">
   <div id="page">
 
-  <div id="header" class="header-menu">
-    <div class="section clearfix">
-	  <div class="stripe">
-        <div class="overbox">
-          <div class="overbox-area">
-            <a href="<?php print $front_page; ?>" title="<?php print t('Cover Page'); ?>" rel="home" id="logo" style="logotype">
-              <img src="<?php print $logo; ?>" alt="<?php print t('SITE NAME'); ?>" />
-            </a>
-            <div class="main-menu">
-              <?php
-                print theme (
-                  'links',
-                  array (
-                    'links' => $main_menu,
-                    'attributes' => array (
-                      'id' => 'main-menu-links',
-                      'class' => array ('links', 'clearfix'),
-                    ),
-                    'heading' => array (
-                      'text' => t('Navigation'),
-                      'level' => 'h2',
-                      'class' => array('element-invisible'),
-                    ),
-                  )
-                );
-              ?>
+  <div id="header-wrapper" class="wrapper">
+    <div id="header-wrap" class="wrap">
+      <div id="header" class="header-menu fixedwrap">
+        <div class="section clearfix">
+          <div class="stripe">
+            <div class="overbox">
+              <div class="overbox-area">
+                <a href="<?php print $front_page; ?>" title="<?php print t('Cover Page'); ?>" rel="home" id="logo" style="logotype">
+                  <img src="<?php print $logo; ?>" alt="<?php print t('SITE NAME'); ?>" />
+                </a>
+                <div class="main-menu">
+                  <?php
+                    print theme (
+                      'links',
+                      array (
+                        'links' => $main_menu,
+                        'attributes' => array (
+                          'id' => 'main-menu-links',
+                          'class' => array ('links', 'clearfix'),
+                        ),
+                        'heading' => array (
+                          'text' => t('Navigation'),
+                          'level' => 'h2',
+                          'class' => array('element-invisible'),
+                        ),
+                      )
+                    );
+                  ?>
+                </div>
+              </div>
             </div>
           </div>
+
+          <div class="header-cover-interlayer bg-filled"></div>
+
+          <!-- < ? php print render($page['header']); ? > -->
+
         </div>
-      </div>
 
-    <div class="header-cover-interlayer bg-filled"></div>
+      </div> <!-- /.section, /#header -->
 
-    <!-- < ? php print render($page['header']); ? > -->
-
-
-
-
-  </div></div> <!-- /.section, /#header -->
+    </div> <!-- wrap -->
+  </div> <!-- wrapper -->
   
   <?php if ($site_name || $site_slogan): ?>
   <div id="sub-header" class="header-cover">
@@ -60,8 +64,11 @@
   </div>
   <?php endif; ?>
 
+a
+aaaaaaa
 
 
-  </div></div> <!-- /.section, /#footer-wrapper -->
+aaaaaaa
+
 
 </div></div> <!-- /#page, /#page-wrapper -->
